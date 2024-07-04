4af3eeacb952d7e3464ccc69478786033fb0686b
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-04 17:00:52 +0200

    feat: Bonus-- Create jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,25 @@
+<script>
+    export default{
+        name: "AppHero"
+    }
+</script>
+
+<template>
+    <div class="jumbotron">
+        <h1>La mia prima app con Vite!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae pariatur veritatis officiis vel neque. Repudiandae earum dicta nemo ad repellat doloribus incidunt quis perspiciatis amet dolorum officiis quos, excepturi odit.</p>
+    </div>
+</template>
+
+<style>
+    .jumbotron{
+        width: 70%;
+        height: 500px;
+        color: violet;
+        background-image: url(../../public/black-1072366_1280.jpg);
+        background-size: cover;
+        background-repeat: repeat;
+        text-align: center;
+        padding: 30px;
+    }
+</style>
